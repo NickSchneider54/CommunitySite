442a7e55d5893009d8c26d17e5dcda3b975144f8
Caleb <whitlowc@otc.edu>
2019-09-25 08:51:02 -0500

    Added base newsletter function
    For authentication/test

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+
+/******* Newsletter */
+function newsletter(frm) {
+    var isValid = true;
+
+    if (frm.Email.value == "" || frm.Email.value == "" || frm.checkbox.value == "") {
+        
+    }
+}
